e0b57524e89212d527f0f03ea6e1a4aa774b1785
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-07 16:08:40 +0100

    Struct (Episode 9)

---
+++ src/bin/struct.rs
@@ -0,0 +1,56 @@
+// EPISODE 9
+
+#![allow(unused)]
+
+// Struct
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+struct Point3d(f32, f32, f32);
+
+struct Empty;
+
+#[derive(Debug)]
+struct Circle {
+    center: Point,
+    radius: u32,
+}
+
+fn main() {
+    // Create
+    let p = Point { x: 1.0, y: 2.0 };
+    println!("point.x = {}, point.y = {}", p.x, p.y);
+
+    let p = Point3d(1.0, 2.0, 3.0);
+    println!("point 3d, {}, {}, {}", p.0, p.1, p.2);
+
+    let empty = Empty;
+
+    let circle = Circle {
+        center: Point { x: 1.0, y: 2.0 },
+        radius: 1,
+    };
+
+    // Debug
+    // Read
+    println!("circle: {:?}", circle);
+
+    // Shortcut
+    let x = 1.0;
+    let y = 1.0;
+    let p = Point { x, y }; // Same as Point { x: x, y: y } since the variables are the same name
+
+    // Copy fields
+    let p0 = Point { x: 1.0, y: 1.0 };
+    let p1 = Point { x: 2.0, ..p0 }; // Same as  Point { x: 2.0, y: p0.y }
+    println!("{:?}", p1);
+
+    // Update
+    let mut p = Point { x: 0.0, y: 0.0 };
+    p.x += 1.0;
+    p.y += 1.0;
+    println!("{:?}", p);
+}
